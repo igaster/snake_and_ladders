eb818e8cc1c6bb09762ff6ba89ab194d1e5b9652
Giannis Gasteratos <igasteratos@gmail.com>
2025-12-22 17:41:39 +0200

    grid with labels

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -3,11 +3,12 @@ import random
 import sys
 
 # Settings
-CELL = 50
+CELL = 50 # Μέγεθος από κάθε τετράγωνο
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (180, 180, 180)
 
 # Initialize pygame
 pygame.init()
@@ -18,7 +19,17 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Φιδάκια και Σκάλες")
 
 # Font
-font = pygame.font.SysFont(None, 48)
+font = pygame.font.SysFont(None, 24)
+
+# Βοηθητική συνάρτηση δέχεται μία θέση του παιχνιδιού (1-100)
+# και επιστρέφει τις συντεταγμένες (x,y) του κέντρου
+def index_to_grid(index):
+    index = index -1
+    y = 9 - (index // 10)
+    x = index % 10
+    if y % 2 == 0:
+        x = 9 - x
+    return x * CELL + CELL // 2, y * CELL + CELL // 2
 
 # Main loop
 running = True
@@ -35,8 +46,13 @@ while running:
     screen.fill(WHITE)
 
     for i in range(1, 11):
-        pygame.draw.line(screen, BLACK, (i * CELL, 0), (i * CELL, 500), 3)
-        pygame.draw.line(screen, BLACK, (0, i * CELL), (500, i * CELL), 3)
+        pygame.draw.line(screen, BLACK, (i * CELL, 0), (i * CELL, 10 * CELL), 3)
+        pygame.draw.line(screen, BLACK, (0, i * CELL), (10 * CELL, i * CELL), 3)
+
+    for i in range(1, 101):
+        text_surface = font.render(str(i), True, GRAY)
+        text_rect = text_surface.get_rect(center=index_to_grid(i))
+        screen.blit(text_surface, text_rect)
 
     # # Γραμμή
     # pygame.draw.line(screen, BLACK, (100, 100), (600, 100), 3)
@@ -46,7 +62,9 @@ while running:
     #
     # # Ορθογώνιο, με γέμισμα
     # pygame.draw.rect(screen, BLACK, (130, 120, 40, 80))
-
+    #
+    # # Κύκλος
+    # pygame.draw.circle(screen, BLACK, (100, 100), 14,  1)
 
     pygame.display.flip()
 
